4427c4861638d74884224371fdcd496085f39919
alexdominguez <alexepsilon98@gmail.com>
2023-03-09 16:55:17 +0100

    Class CarSet and Main ready for first test, i've not run it already

--- src/main/java/SmartCity/CarSet.java
+++ src/main/java/SmartCity/CarSet.java
@@ -1,5 +1,6 @@
 package SmartCity;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.Set;
@@ -13,25 +14,28 @@ public class CarSet {
 
     //Empty constructor
     public CarSet(){
-        this.CS = new Set<new Car>;
+        this.CS = new HashSet<>();
     }
 
     //1 to 1 constructor
 
-    public void fillRand_CarSet(int n){
-        for (int i = 0; i < n; ++i){
-            int x0, y0, x1, y1;
-            x0 = getRandomNumber(0, 100);
-            y0 = getRandomNumber(0, 100);
-            x1 = getRandomNumber(0, 100);
-            y1 = getRandomNumber(0, 100);
-            Car aux = new Car(new Pos(x0,y0), new Pos(x1,y1));
-            this.CS.add(aux);
+    public void organize(Set<int[]> poblation, int nDrivers){
+        if (poblation.size() == nDrivers) {
+            for (int[] p : poblation) {
+                Car aux = new Car(new Pos(p[0], p[1]), new Pos(p[2], p[3]));
+                this.CS.add(aux);
+            }
+        }
+    }
+
+    public void execute(){
+        for (Car c : this.CS){
+            c.moveTo(c.getDest());
         }
     }
 
     public void print_CarSet(){
-        for (Car c : CS) {
+        for (Car c : this.CS) {
             System.out.println(c.getPos());
             System.out.println(c.getDest());
             System.out.println(c.getRemTime());
--- src/main/java/SmartCity/Main.java
+++ src/main/java/SmartCity/Main.java
@@ -1,16 +1,37 @@
 package SmartCity;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner n = new Scanner(System.in);
-
         //Inicialize CarSet with n random Cars
         CarSet CS = new CarSet();
-        CS.fillRand_CarSet(n.nextInt());
-
-
+        Scanner n = new Scanner(System.in);
+        System.out.println("How many people do you to organize?");
+        Set<int[]> poblation = getPoblationN(n.nextInt());
+        System.out.println("How many drivers do you have?");
+        CS.organize(poblation, n.nextInt());
+        CS.execute();
+        CS.print_CarSet();
     }
+
+    private static Set<int[]> getPoblationN(int n){
+        Set<int[]> poblation = new HashSet<>();
+        for (int i = 0; i < n; ++i){
+            int[] p = new int[4];
+            for (int j = 0; j < n; ++j){
+                p[j] = getRandomNumber(0, 100);
+            }
+            poblation.add(p);
+        }
+        return poblation;
+    }
+
+    private static int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+
 }
